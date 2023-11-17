c1d5ba51012e52701dda44bbe6508877a476ee06
NikPeg <peganov.nik@gmail.com>
2023-11-17 18:54:52 +0300

    Normal openai migrate

--- proxy.py
+++ proxy.py
@@ -1,22 +1,23 @@
 from config import OPENAI_API_KEY
 from openai import OpenAI
 
-client = OpenAI(api_key=OPENAI_API_KEY)
 from tenacity import retry, wait_random_exponential, wait_fixed, stop_after_attempt
 
 
 class GPTProxy:
     def __init__(self, model="gpt-3.5-turbo"):
-        
+        self.client = OpenAI(api_key=OPENAI_API_KEY)
         self.model = model
 
     @retry(wait=wait_fixed(21), stop=stop_after_attempt(10))
     def ask(self, message):
         try:
-            completion = client.chat.completions.create(model=self.model,
-            messages=[
-                {"role": "user", "content": message}
-            ])
+            completion = self.client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "user", "content": message}
+                ]
+            )
 
             return completion.choices[0].message.content
         except Exception as e:
